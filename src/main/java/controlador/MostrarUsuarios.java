232103a0f5c2b7bc64f6af70d12d03c124cbd0e8
O-Gerler <azurmendi5o@outlook.com>
2023-05-02 13:23:58 +0200

    ahora se muestran los usuarios

---
+++ src/main/java/controlador/MostrarUsuarios.java
@@ -0,0 +1,54 @@
+package controlador;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import modeloDAO.ModeloUsuario;
+import modeloDTO.Usuario;
+
+/**
+ * Servlet implementation class MostrarUsuarios
+ */
+@WebServlet("/MostrarUsuarios")
+public class MostrarUsuarios extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MostrarUsuarios() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		ModeloUsuario modeloUsuario = new ModeloUsuario();
+		modeloUsuario.conectar();
+		
+		ArrayList<Usuario> usuarios = modeloUsuario.getAllUsuarios();
+		
+		modeloUsuario.cerrar();
+		
+		request.setAttribute("usuarios", usuarios);
+		request.getRequestDispatcher("usuarios/verUsuarios.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
