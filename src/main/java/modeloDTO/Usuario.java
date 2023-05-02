8b1259fdab3271647347ee84e97856cb747bce18
O-Gerler <azurmendi5o@outlook.com>
2023-05-02 12:59:21 +0200

    ahora telefono es un string

--- src/main/java/modeloDTO/Usuario.java
+++ src/main/java/modeloDTO/Usuario.java
@@ -10,7 +10,7 @@ public class Usuario {
 	private String dni;
 	private String email;
 	private String contrasena = "1234";
-	private int telefono;
+	private String telefono;
 	private Date fechaNacimiento;
 	private ArrayList<Vehiculo> vehiculos = new ArrayList<>();
 	
@@ -50,10 +50,10 @@ public class Usuario {
 	public void setContrasena(String contrasena) {
 		this.contrasena = contrasena;
 	}
-	public int getTelefono() {
+	public String getTelefono() {
 		return telefono;
 	}
-	public void setTelefono(int telefono) {
+	public void setTelefono(String telefono) {
 		this.telefono = telefono;
 	}
 	public Date getFechaNacimiento() {
