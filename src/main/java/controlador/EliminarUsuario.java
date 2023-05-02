16da14eaae86fa0be63f0e9ee5bc76cafb18af93
O-Gerler <azurmendi5o@outlook.com>
2023-05-02 21:50:05 +0200

    eliminar usuario

---
+++ src/main/java/controlador/EliminarUsuario.java
@@ -0,0 +1,52 @@
+package controlador;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import modeloDAO.ModeloUsuario;
+
+/**
+ * Servlet implementation class EliminarUsuario
+ */
+@WebServlet("/EliminarUsuario")
+public class EliminarUsuario extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EliminarUsuario() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		int id = Integer.parseInt(request.getParameter("id"));
+		
+		ModeloUsuario modeloUsuario = new ModeloUsuario();
+		modeloUsuario.conectar();
+		
+		modeloUsuario.eliminarUsuario(id);
+		
+		modeloUsuario.cerrar();
+		
+		request.getRequestDispatcher("MostrarUsuarios").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
