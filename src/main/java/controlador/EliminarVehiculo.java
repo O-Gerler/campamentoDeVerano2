e650eb924667719c16e398aa9b47cce5d4b73263
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-05-03 12:34:35 +0200

    eliminar vehiculo

---
+++ src/main/java/controlador/EliminarVehiculo.java
@@ -0,0 +1,52 @@
+package controlador;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import modeloDAO.ModeloVehiculos;
+
+/**
+ * Servlet implementation class EliminarVehiculo
+ */
+@WebServlet("/EliminarVehiculo")
+public class EliminarVehiculo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EliminarVehiculo() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		int id = Integer.parseInt(request.getParameter("id"));
+		
+		ModeloVehiculos modeloVehiculos = new ModeloVehiculos();
+		modeloVehiculos.conectar();
+		
+		modeloVehiculos.eliminarVehiculo(id);
+		
+		modeloVehiculos.cerrar();
+		
+		request.getRequestDispatcher("MostrarVehiculos").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
