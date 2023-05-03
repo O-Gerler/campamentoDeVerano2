f277b3c7f93e3147b66b43ed04e8b10e244455bb
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-05-03 09:03:56 +0200

    muestra los tipos

---
+++ src/main/java/controlador/MostrarTipos.java
@@ -0,0 +1,54 @@
+package controlador;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import modeloDAO.ModeloTipo;
+import modeloDTO.Tipo;
+
+/**
+ * Servlet implementation class MostrarTipos
+ */
+@WebServlet("/MostrarTipos")
+public class MostrarTipos extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MostrarTipos() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		ModeloTipo modeloTipo = new ModeloTipo();
+		modeloTipo.conectar();
+		
+		ArrayList<Tipo> tipos = modeloTipo.getAllTipos();
+		
+		modeloTipo.cerrar();
+		
+		request.setAttribute("tipos", tipos);
+		request.getRequestDispatcher("tipos/verTipos.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
