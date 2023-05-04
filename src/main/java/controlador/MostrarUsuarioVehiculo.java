5614602cc5677556064d14635e5ee0b45f0138ef
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-05-04 10:56:31 +0200

    muestra los usuarios con los vehiculos

---
+++ src/main/java/controlador/MostrarUsuarioVehiculo.java
@@ -0,0 +1,54 @@
+package controlador;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import modeloDAO.ModeloUsuarioVehiculo;
+import modeloDTO.UsuarioVehiculo;
+
+/**
+ * Servlet implementation class MostrarUsuarioVehiculo
+ */
+@WebServlet("/MostrarUsuarioVehiculo")
+public class MostrarUsuarioVehiculo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MostrarUsuarioVehiculo() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		ModeloUsuarioVehiculo modeloUsuarioVehiculo = new ModeloUsuarioVehiculo();
+		modeloUsuarioVehiculo.conectar();
+		
+		ArrayList<UsuarioVehiculo> usuarioVehiculos = modeloUsuarioVehiculo.getAllUsuarioVehiculoIDUsuario();
+		
+		modeloUsuarioVehiculo.cerrar();
+		
+		request.setAttribute("usuarioVehiculos", usuarioVehiculos);
+		request.getRequestDispatcher("usuariosVehiculos/ver.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
