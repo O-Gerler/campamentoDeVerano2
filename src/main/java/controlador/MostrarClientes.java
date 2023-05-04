f65c4534af408e46191b29ec4740f910033db21f
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-05-04 12:47:02 +0200

    muestra los clientes

--- src/main/java/controlador/MostrarClientes.java
+++ src/main/java/controlador/MostrarClientes.java
@@ -1,11 +1,16 @@
 package controlador;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import modeloDAO.ModeloCliente;
+import modeloDTO.Cliente;
 
 /**
  * Servlet implementation class MostrarClientes
@@ -27,7 +32,15 @@ public class MostrarClientes extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		ModeloCliente modeloCliente = new ModeloCliente();
+		modeloCliente.conectar();
+		
+		ArrayList<Cliente> clientes = modeloCliente.getAllClientes();
+		
+		modeloCliente.cerrar();
+		
+		request.setAttribute("clientes", clientes);
+		request.getRequestDispatcher("usuarios/verUsuarios.jsp").forward(request, response);
 	}
 
 	/**
