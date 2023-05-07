416394b873e1ba211038d77fcaf72e3f12194f81
O-Gerler <azurmendi5o@outlook.com>
2023-05-07 15:58:08 +0200

    insertar cliente

---
+++ src/main/java/controlador/InsertarCliente.java
@@ -0,0 +1,89 @@
+package controlador;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import modeloDAO.ModeloCliente;
+import modeloDAO.ModeloGrupo;
+import modeloDAO.ModeloUsuario;
+import modeloDTO.Cliente;
+import modeloDTO.Grupo;
+import modeloDTO.Usuario;
+
+/**
+ * Servlet implementation class InsertarCliente
+ */
+@WebServlet("/InsertarCliente")
+public class InsertarCliente extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public InsertarCliente() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		ModeloUsuario modeloUsuario = new ModeloUsuario();
+		ModeloGrupo modeloGrupo = new ModeloGrupo();
+		
+		modeloGrupo.conectar();
+		modeloUsuario.conectar();
+		
+		ArrayList<Usuario> usuarios = modeloUsuario.getAllUsuarios();
+		ArrayList<Grupo> grupos = modeloGrupo.getAllGrupos();
+		
+		modeloGrupo.cerrar();
+		modeloUsuario.cerrar();
+		
+		request.setAttribute("grupos", grupos);
+		request.setAttribute("usuarios", usuarios);
+		request.getRequestDispatcher("clientes/insertarCliente.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		int id_cliente = Integer.parseInt(request.getParameter("id_cliente"));
+		int id_grupo = Integer.parseInt(request.getParameter("id_grupo"));
+		
+		ModeloUsuario modeloUsuario = new ModeloUsuario();
+		ModeloGrupo modeloGrupo = new ModeloGrupo();
+		
+		modeloGrupo.conectar();
+		modeloUsuario.conectar();
+		
+		Usuario usuario = modeloUsuario.getUsuarios(id_cliente);
+		Grupo grupo = modeloGrupo.getGrupo(id_grupo);
+		
+		modeloGrupo.cerrar();
+		modeloUsuario.cerrar();
+		
+		ModeloCliente modeloCliente = new ModeloCliente();
+		
+		Cliente cliente = new Cliente();
+		
+		cliente.setId(usuario.getId());
+		cliente.setGrupo(grupo);
+		
+		modeloCliente.insertarCliente(cliente);
+		modeloCliente.cerrar();
+		
+		request.getRequestDispatcher("MostrarClientes").forward(request, response);
+	}
+
+}
