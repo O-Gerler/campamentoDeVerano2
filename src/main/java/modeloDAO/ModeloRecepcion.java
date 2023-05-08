67a28deebee7b6c0214235d1cc97589ddecae7e7
O-Gerler <azurmendi5o@outlook.com>
2023-05-08 19:46:41 +0200

    metodo getRecepcion

--- src/main/java/modeloDAO/ModeloRecepcion.java
+++ src/main/java/modeloDAO/ModeloRecepcion.java
@@ -1,12 +1,37 @@
 package modeloDAO;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import modeloDTO.Recepcionista;
+import modeloDTO.Personal;
+import modeloDTO.Recepcion;
 
 public class ModeloRecepcion extends Conector{
-	public boolean insertarRecepcion(Recepcionista recepcionista) {
+	private Recepcion recepcionHeredaUsuario(int id) {
+		ModeloPersonal modeloPersonal = new ModeloPersonal();
+		modeloPersonal.conectar();
+		
+		Personal personal = modeloPersonal.getPersonal(id);
+		
+		Recepcion recepcion = new Recepcion();
+
+		recepcion.setId(personal.getId());
+		recepcion.setNombre(personal.getNombre());
+		recepcion.setApellido(personal.getApellido());
+		recepcion.setDni(personal.getDni());
+		recepcion.setContrasena(personal.getContrasena());
+		recepcion.setEmail(personal.getEmail());
+		recepcion.setFechaNacimiento(personal.getFechaNacimiento());
+		recepcion.setTelefono(personal.getTelefono());
+		recepcion.setVehiculos(personal.getVehiculos());
+		recepcion.setDirector(personal.getDirector());
+		recepcion.setFechaIngreso(personal.getFechaIngreso());
+		
+		return recepcion;
+	}
+	
+	public boolean insertarRecepcion(Recepcion recepcionista) {
 		String st = "INSERT INTO recepcion VALUES (?)";
 		
 		try {
@@ -40,5 +65,23 @@ public class ModeloRecepcion extends Conector{
 		}
 		
 		return false;
+	}
+	
+	public Recepcion getRecepcion(int id) {
+		String st = "SELECT * FROM recepcion WHERE id_recepcion=?";
+		
+		try {
+			PreparedStatement pst = super.connection.prepareStatement(st);
+			
+			pst.setInt(1, id);
+			
+			ResultSet rs = pst.executeQuery();
+			rs.next();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
 	}
 }
