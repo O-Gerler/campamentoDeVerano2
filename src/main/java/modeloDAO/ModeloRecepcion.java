2358a082f971d56173991bca06f7496fb39718b7
O-Gerler <azurmendi5o@outlook.com>
2023-05-08 19:33:17 +0200

    metodo insertar

--- src/main/java/modeloDAO/ModeloRecepcion.java
+++ src/main/java/modeloDAO/ModeloRecepcion.java
@@ -1,5 +1,26 @@
 package modeloDAO;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import modeloDTO.Recepcionista;
+
 public class ModeloRecepcion extends Conector{
-	
+	public boolean insertarRecepcion(Recepcionista recepcionista) {
+		String st = "INSERT INTO recepcion VALUES (?)";
+		
+		try {
+			PreparedStatement pst = super.connection.prepareStatement(st);
+			
+			pst.setInt(1, recepcionista.getId());
+			
+			pst.execute();
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
 }
