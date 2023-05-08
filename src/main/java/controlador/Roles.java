65f4b58c74ce0e5808f2f7672ff61cb02920dec8
O-Gerler <azurmendi5o@outlook.com>
2023-05-08 19:10:02 +0200

    usuario y contrasena de admin puesta

--- src/main/java/controlador/Roles.java
+++ src/main/java/controlador/Roles.java
@@ -5,4 +5,6 @@ public interface Roles {
 	final static int LIMPIEZA = 2;
 	final static int RECEPCION = 3;
 	final static int ADMIN = 4;
+	final static String USER_ADMIN = "admin";
+	final static String PASSWORD_ADMIN = "admin";
 }
