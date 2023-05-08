2cdc934b299dc18ec60860837c987dc03ebf6943
O-Gerler <azurmendi5o@outlook.com>
2023-05-08 22:50:40 +0200

    sesion agregada

--- src/main/java/controlador/VistaRecepcion.java
+++ src/main/java/controlador/VistaRecepcion.java
@@ -1,12 +1,18 @@
 package controlador;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import modeloDAO.ModeloCliente;
+import modeloDTO.Cliente;
+import modeloDTO.Recepcion;
 
 /**
  * Servlet implementation class VistaRecepcion
@@ -28,7 +34,20 @@ public class VistaRecepcion extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		HttpSession sesion = request.getSession(); 
+		HttpSession sesion = request.getSession();
+		
+		Recepcion recepcion = (Recepcion) sesion.getAttribute("recepcion");
+		
+		if (recepcion != null) {
+			ModeloCliente modeloCliente = new ModeloCliente();
+			modeloCliente.conectar();
+			
+			ArrayList<Cliente> clientes = modeloCliente.getAllClientes();
+			
+			request.setAttribute("clientes", clientes);
+			
+			request.getRequestDispatcher("vistaRecepcion/vistaRecepcion.jsp").forward(request, response);
+		}
 	}
 
 	/**
