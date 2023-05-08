f7f65e4abb936c0555a9047ecee49c47aa4b7acd
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-05-08 11:42:05 +0200

    cierre de sesion

--- src/main/java/controlador/CerrarSesion.java
+++ src/main/java/controlador/CerrarSesion.java
@@ -6,6 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class CerrarSesion
@@ -27,7 +28,9 @@ public class CerrarSesion extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		HttpSession sesion = request.getSession();
+	    sesion.invalidate();
+	    response.sendRedirect("/MostrarLogin");
 	}
 
 	/**
