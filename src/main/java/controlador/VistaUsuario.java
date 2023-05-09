7ea4427927994673aacd2499aa8fe71801d67c47
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-05-09 12:24:04 +0200

    first commit

---
+++ src/main/java/controlador/VistaUsuario.java
@@ -0,0 +1,51 @@
+package controlador;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import modeloDTO.Usuario;
+
+/**
+ * Servlet implementation class VistaUsuario
+ */
+@WebServlet("/VistaUsuario")
+public class VistaUsuario extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public VistaUsuario() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+		
+		Usuario usuario = (Usuario) session.getAttribute("usuario");
+		
+		if (usuario != null) {
+			request.setAttribute("usuario", usuario);
+			request.getRequestDispatcher("vistaUsuario/vistaUsuario.jsp");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
