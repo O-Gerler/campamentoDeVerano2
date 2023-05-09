fc8314ec396909e6480e990426e85600bb5fed53
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-05-09 13:20:57 +0200

    do post insertando reserva basica sin restricciones

--- src/main/java/controlador/InsertarReserva.java
+++ src/main/java/controlador/InsertarReserva.java
@@ -1,7 +1,10 @@
 package controlador;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,9 +14,11 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import modeloDAO.ModeloParcela;
+import modeloDAO.ModeloReserva;
 import modeloDAO.ModeloUsuario;
 import modeloDTO.Parcela;
 import modeloDTO.Recepcion;
+import modeloDTO.Reserva;
 import modeloDTO.Usuario;
 
 /**
@@ -40,6 +45,8 @@ public class InsertarReserva extends HttpServlet {
 		
 		Recepcion recepcion = (Recepcion) session.getAttribute("recepcion");
 		
+		Usuario usuario = (Usuario) session.getAttribute("usuario");
+		
 		if(recepcion != null) {
 			ModeloUsuario modeloUsuario = new ModeloUsuario();
 			modeloUsuario.conectar();
@@ -59,14 +66,87 @@ public class InsertarReserva extends HttpServlet {
 			request.setAttribute("parcelas", parcelas);
 			request.getRequestDispatcher("reservas/insertarReserva.jsp").forward(request, response);
 		}
+		
+		if(usuario != null) {
+			ModeloParcela modeloParcela = new ModeloParcela();
+			modeloParcela.conectar();
+			
+			ArrayList<Parcela> parcelas = modeloParcela.getAllParcelas();
+			
+			modeloParcela.cerrar();
+			
+			request.setAttribute("usuario", usuario);
+			request.setAttribute("parcelas", parcelas);
+			request.getRequestDispatcher("reservas/insertarReserva.jsp").forward(request, response);
+		}
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		int id_usuario = Integer.parseInt(request.getParameter("id_usuario"));
+		int id_parcela = Integer.parseInt(request.getParameter("id_parcela"));
+		String fecha_ingreso = request.getParameter("fecha_ingreso");
+		String fecha_salida = request.getParameter("fecha_salida");
+		
+		Date fechaIngreso = null;
+		
+		try {
+			fechaIngreso = new SimpleDateFormat("yyyy-MM-dd").parse(fecha_ingreso);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Date fechaSalida = null;
+		
+		try {
+			fechaSalida = new SimpleDateFormat("yyyy-MM-dd").parse(fecha_salida);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		ModeloUsuario modeloUsuario = new ModeloUsuario();
+		modeloUsuario.conectar();
+		
+		Usuario usuario = modeloUsuario.getUsuarios(id_usuario);
+		
+		modeloUsuario.cerrar();
+		
+		ModeloParcela modeloParcela = new ModeloParcela();
+		modeloParcela.conectar();
+		
+		Parcela parcela = modeloParcela.getParcela(id_parcela);
+		
+		modeloParcela.cerrar();
+		
+		Reserva reserva = new Reserva();
+		
+		reserva.setUsuario(usuario);
+		reserva.setParcela(parcela);
+		reserva.setFecha_ingreso(fechaIngreso);
+		reserva.setFecha_salida(fechaSalida);
+		
+		ModeloReserva modeloReserva = new ModeloReserva();
+		modeloReserva.conectar();
+		
+		modeloReserva.insertarReserva(reserva);
+		
+		modeloReserva.cerrar();
+		
+		HttpSession session = request.getSession();
+		
+		Recepcion VistaRecepcion = (Recepcion) session.getAttribute("recepcion");
+		
+		Usuario VistaUsuario = (Usuario) session.getAttribute("usuario");
+		
+		if(VistaRecepcion != null) {
+			request.getRequestDispatcher("VistaRecepcion").forward(request, response);
+		}if(VistaUsuario != null) {
+			request.getRequestDispatcher("VistaUsuario").forward(request, response);
+		}
 	}
 
 }
