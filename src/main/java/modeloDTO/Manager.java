389a55fbf83ec630ace52666237f5b285cbb0300
O-Gerler <azurmendi5o@outlook.com>
2023-05-10 16:57:22 +0200

    user y password de manager

--- src/main/java/modeloDTO/Manager.java
+++ src/main/java/modeloDTO/Manager.java
@@ -1,5 +1,8 @@
 package modeloDTO;
 
-public class Manager extends Personal {
+import controlador.Roles;
 
+public class Manager implements Roles{
+	private static String usuario = Roles.USER_ADMIN;
+	private static String contrasena = Roles.PASSWORD_ADMIN;
 }
