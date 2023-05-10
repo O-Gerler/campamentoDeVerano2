cbe93bd02ffd891172e7e07b7faaf3319b645601
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-05-10 12:46:32 +0200

    servlet de limpiar parcela

--- src/main/java/controlador/LimpiarParcela.java
+++ src/main/java/controlador/LimpiarParcela.java
@@ -6,6 +6,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import modeloDAO.ModeloParcela;
+import modeloDTO.Limpieza;
 
 /**
  * Servlet implementation class LimpiarParcela
@@ -27,7 +31,26 @@ public class LimpiarParcela extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		HttpSession session = request.getSession();
+		
+		Limpieza limpieza = (Limpieza) session.getAttribute("limpieza");
+		
+		if (limpieza != null) {
+			int id_parcela = Integer.parseInt(request.getParameter("id_parcela"));
+			
+			ModeloParcela modeloParcela = new ModeloParcela();
+			modeloParcela.conectar();
+			
+			modeloParcela.limpiarParcela(id_parcela);
+			
+			modeloParcela.cerrar();
+			
+			response.sendRedirect("VistaLimpieza");
+		}else {
+			request.getRequestDispatcher("error404.jsp").forward(request, response);
+		}
+		
+		
 	}
 
 	/**
