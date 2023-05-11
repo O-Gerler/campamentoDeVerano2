5b998d845a733a07fed24ec8a2070a2d13924f86
O-Gerler <azurmendi5o@outlook.com>
2023-05-11 18:55:07 +0200

    inserta un monitor

--- src/main/java/controlador/InsertarMonitor.java
+++ src/main/java/controlador/InsertarMonitor.java
@@ -1,11 +1,19 @@
 package controlador;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import modeloDAO.ModeloMonitor;
+import modeloDAO.ModeloPersonal;
+import modeloDTO.Manager;
+import modeloDTO.Personal;
 
 /**
  * Servlet implementation class InsertarMonitor
@@ -27,15 +35,47 @@ public class InsertarMonitor extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		HttpSession session = request.getSession();
+		
+		Manager manager = (Manager) session.getAttribute("manager");
+		
+		if (manager != null) {
+			ModeloPersonal modeloPersonal = new ModeloPersonal();
+			modeloPersonal.conectar();
+			
+			ArrayList<Personal> personales = modeloPersonal.getPersonalSinRol();
+			
+			modeloPersonal.cerrar();
+			
+			request.setAttribute("personales", personales);
+			request.getRequestDispatcher("monitores/insertarMonitor.jsp").forward(request, response);
+		}else {
+			request.getRequestDispatcher("error404.jsp").forward(request, response);
+		}
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		int id_monitor = Integer.parseInt(request.getParameter("id_monitor"));
+		
+		ModeloMonitor modeloMonitor = new ModeloMonitor();
+		modeloMonitor.conectar();
+		
+		modeloMonitor.insertarMonitor(id_monitor);
+		
+		modeloMonitor.cerrar();
+		
+		HttpSession session = request.getSession();
+		
+		Manager manager = (Manager) session.getAttribute("manager");
+		
+		if (manager != null) {
+			response.sendRedirect("VistaManager");
+		}else {
+			request.getRequestDispatcher("error404.jsp").forward(request, response);
+		}
 	}
 
 }
