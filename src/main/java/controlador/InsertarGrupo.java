119d049fc5fa9b22b38429f565e914a78a9b638c
O-Gerler <azurmendi5o@outlook.com>
2023-05-12 01:05:05 +0200

    insertarGrupo

--- src/main/java/controlador/InsertarGrupo.java
+++ src/main/java/controlador/InsertarGrupo.java
@@ -1,11 +1,20 @@
 package controlador;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import modeloDAO.ModeloGrupo;
+import modeloDAO.ModeloMonitor;
+import modeloDTO.Grupo;
+import modeloDTO.Manager;
+import modeloDTO.Monitor;
 
 /**
  * Servlet implementation class InsertarGrupo
@@ -26,16 +35,47 @@ public class InsertarGrupo extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		HttpSession session = request.getSession();
+		
+		Manager manager = (Manager) session.getAttribute("manager");
+		
+		if (manager != null) {
+			ModeloMonitor modeloMonitor = new ModeloMonitor();
+			modeloMonitor.conectar();
+			
+			ArrayList<Monitor> monitores = modeloMonitor.getMonitoresSinGrupo();
+			
+			modeloMonitor.cerrar();
+			
+			request.setAttribute("monitores", monitores);
+			request.getRequestDispatcher("grupos/insertarGrupo.jsp").forward(request, response);
+		}else {
+			request.getRequestDispatcher("error404.jsp").forward(request, response);
+		}
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		int id_monitor = Integer.parseInt(request.getParameter("id_monitor"));
+		
+		ModeloGrupo modeloGrupo = new ModeloGrupo();
+		modeloGrupo.conectar();
+		
+		modeloGrupo.insertarGrupo(id_monitor);
+		
+		modeloGrupo.cerrar();
+		
+		HttpSession session = request.getSession();
+		
+		Manager manager = (Manager) session.getAttribute("manager");
+		
+		if (manager != null) {
+			response.sendRedirect("VistaManager");
+		}else {
+			request.getRequestDispatcher("error404.jsp").forward(request, response);
+		}
 	}
 
 }
