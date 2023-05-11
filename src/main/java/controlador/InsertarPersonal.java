42b49d045386394816a35aaf0427cc3577ec3b4e
plaiaundi <plaiaundi@HZ309812.H012108.NET>
2023-05-11 12:30:12 +0200

    inserta un usuario como personal

--- src/main/java/controlador/InsertarPersonal.java
+++ src/main/java/controlador/InsertarPersonal.java
@@ -1,11 +1,23 @@
 package controlador;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import modeloDAO.ModeloPersonal;
+import modeloDAO.ModeloUsuario;
+import modeloDTO.Manager;
+import modeloDTO.Personal;
+import modeloDTO.Usuario;
 
 /**
  * Servlet implementation class InsertarPersonal
@@ -26,16 +38,72 @@ public class InsertarPersonal extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		HttpSession session = request.getSession();
+		
+		Manager manager = (Manager) session.getAttribute("manager");
+		
+		if (manager != null) {
+			ModeloUsuario modeloUsuario = new ModeloUsuario();
+			modeloUsuario.conectar();
+			
+			ArrayList<Usuario> usuarios = modeloUsuario.getUsuariosNoPersonal();
+			
+			modeloUsuario.cerrar();
+			
+			ModeloPersonal modeloPersonal = new ModeloPersonal();
+			modeloPersonal.conectar();
+			
+			ArrayList<Personal> personales = modeloPersonal.getAllPersonal();
+			
+			modeloPersonal.cerrar();
+			
+			request.setAttribute("usuarios", usuarios);
+			request.setAttribute("personales", personales);
+			request.getRequestDispatcher("personal/insertarPersonal.jsp").forward(request, response);
+		}else {
+			request.getRequestDispatcher("error404.jsp").forward(request, response);
+		}
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		int id_usuario = Integer.parseInt(request.getParameter("id_usuario"));
+		int dirige = Integer.parseInt(request.getParameter("dirige"));
+		String fecha_ingreso = request.getParameter("fecha_ingreso");
+		
+		Date fechaIngreso = null;
+		
+		try {
+			fechaIngreso = new SimpleDateFormat("yyyy-MM-dd").parse(fecha_ingreso);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Personal personal = new Personal();
+		
+		personal.setId(id_usuario);
+		personal.setDirector(dirige);
+		personal.setFechaIngreso(fechaIngreso);
+		
+		ModeloPersonal modeloPersonal = new ModeloPersonal();
+		modeloPersonal.conectar();
+		
+		modeloPersonal.insertarPersonal(personal);
+		
+		modeloPersonal.cerrar();
+		
+		HttpSession session = request.getSession();
+		
+		Manager manager = (Manager) session.getAttribute("manager");
+		
+		if (manager != null) {
+			response.sendRedirect("VistaManager");
+		}else {
+			request.getRequestDispatcher("error404.jsp").forward(request, response);
+		}
 	}
 
 }
