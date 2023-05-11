70916bad682602c3c4cb1dc1430ea015e32d4331
O-Gerler <azurmendi5o@outlook.com>
2023-05-11 20:14:14 +0200

    modificar limpieza

--- src/main/java/controlador/ModificarLimpieza.java
+++ src/main/java/controlador/ModificarLimpieza.java
@@ -1,11 +1,20 @@
 package controlador;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import modeloDAO.ModeloLimpieza;
+import modeloDAO.ModeloZona;
+import modeloDTO.Limpieza;
+import modeloDTO.Manager;
+import modeloDTO.Zona;
 
 /**
  * Servlet implementation class ModificarLimpieza
@@ -26,16 +35,58 @@ public class ModificarLimpieza extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		HttpSession session = request.getSession();
+		
+		Manager manager = (Manager) session.getAttribute("manager");
+		
+		if (manager != null) {
+			int id = Integer.parseInt(request.getParameter("id"));
+			
+			ModeloLimpieza modeloLimpieza = new ModeloLimpieza();
+			modeloLimpieza.conectar();
+			
+			Limpieza limpieza =  modeloLimpieza.getLimpieza(id);
+			
+			modeloLimpieza.cerrar();
+			
+			ModeloZona modeloZona = new ModeloZona();
+			modeloZona.conectar();
+			
+			ArrayList<Zona> zonas = modeloZona.getAllZonas();
+			
+			modeloZona.cerrar();
+			
+			request.setAttribute("limpieza", limpieza);
+			request.setAttribute("zonas", zonas);
+			request.getRequestDispatcher("limpieza/modificarLimpieza.jsp").forward(request, response);
+		}else {
+			request.getRequestDispatcher("error404.jsp").forward(request, response);
+		}
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		int id_limpieza = Integer.parseInt(request.getParameter("id_limpieza"));
+		int id_zona = Integer.parseInt(request.getParameter("id_zona"));
+		
+		ModeloLimpieza modeloLimpieza = new ModeloLimpieza();
+		modeloLimpieza.conectar();
+		
+		modeloLimpieza.modificarLimpieza(id_limpieza, id_zona);
+		
+		modeloLimpieza.cerrar();
+		
+		HttpSession session = request.getSession();
+		
+		Manager manager = (Manager) session.getAttribute("manager");
+		
+		if (manager != null) {
+			response.sendRedirect("VistaManager");
+		}else {
+			request.getRequestDispatcher("error404.jsp").forward(request, response);
+		}
 	}
 
 }
