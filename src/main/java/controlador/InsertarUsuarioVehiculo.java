df66636511c6fbd3003adf894e71716e1fdf8c0e
O-Gerler <azurmendi5o@outlook.com>
2023-05-12 06:27:59 +0200

    sesiones y error

--- src/main/java/controlador/InsertarUsuarioVehiculo.java
+++ src/main/java/controlador/InsertarUsuarioVehiculo.java
@@ -8,10 +8,12 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import modeloDAO.ModeloUsuario;
 import modeloDAO.ModeloUsuarioVehiculo;
 import modeloDAO.ModeloVehiculos;
+import modeloDTO.Manager;
 import modeloDTO.Usuario;
 import modeloDTO.UsuarioVehiculo;
 import modeloDTO.Vehiculo;
@@ -35,22 +37,30 @@ public class InsertarUsuarioVehiculo extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		ModeloUsuario modeloUsuario = new ModeloUsuario();
-		ModeloVehiculos modeloVehiculos = new ModeloVehiculos();
+		HttpSession session = request.getSession();
 		
-		modeloUsuario.conectar();
-		modeloVehiculos.conectar();
+		Manager manager = (Manager) session.getAttribute("manager");
 		
-		ArrayList<Usuario> usuarios = modeloUsuario.getAllUsuarios();
-		ArrayList<Vehiculo> vehiculos = modeloVehiculos.getAllVehiculos();
+		if (manager != null) {
+			ModeloUsuario modeloUsuario = new ModeloUsuario();
+			ModeloVehiculos modeloVehiculos = new ModeloVehiculos();
+			
+			modeloUsuario.conectar();
+			modeloVehiculos.conectar();
+			
+			ArrayList<Usuario> usuarios = modeloUsuario.getAllUsuarios();
+			ArrayList<Vehiculo> vehiculos = modeloVehiculos.getAllVehiculos();
+			
+			modeloUsuario.cerrar();
+			modeloVehiculos.cerrar();
+			
+			request.setAttribute("vehiculos", vehiculos);
+			request.setAttribute("usuarios", usuarios);
+			request.getRequestDispatcher("usuariosVehiculos/insertar.jsp").forward(request, response);
+		}else {
+			request.getRequestDispatcher("error404.jsp").forward(request, response);
+		}
 		
-		modeloUsuario.cerrar();
-		modeloVehiculos.cerrar();
-		
-		request.setAttribute("vehiculos", vehiculos);
-		request.setAttribute("usuarios", usuarios);
-		request.getRequestDispatcher("usuariosVehiculos/insertar.jsp").forward(request, response);
 	}
 
 	/**
@@ -85,7 +95,12 @@ public class InsertarUsuarioVehiculo extends HttpServlet {
 		
 		modeloUsuarioVehiculo.cerrar();
 		
-		request.getRequestDispatcher("MostrarUsuarioVehiculo").forward(request, response);
+		HttpSession session = request.getSession();
+		
+		Manager manager = (Manager) session.getAttribute("manager");
+		
+		if (manager != null) { response.sendRedirect("VistaManager"); }
+		else { request.getRequestDispatcher("error404.jsp").forward(request, response); }
 	}
 
 }
