2af3dca906cb77b6d9cb2911846b16218425a747
O-Gerler <azurmendi5o@outlook.com>
2023-05-12 06:34:48 +0200

    registro de un usuario y acceso al momento

--- src/main/java/controlador/Registro.java
+++ src/main/java/controlador/Registro.java
@@ -1,11 +1,19 @@
 package controlador;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import modeloDAO.ModeloUsuario;
+import modeloDTO.Usuario;
 
 /**
  * Servlet implementation class Registro
@@ -26,16 +34,61 @@ public class Registro extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("login/registro.jsp").forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String nombre = request.getParameter("nombre");
+		String apellidos = request.getParameter("apellidos");
+		String dni = request.getParameter("dni");
+		String email = request.getParameter("email");
+		String contrasena = request.getParameter("contrasena");
+		String telf = request.getParameter("telf");
+		String fecha = request.getParameter("fecha_nacimiento");
+		
+		System.out.println(fecha);
+		
+		Date fechaNacimiento = null;
+		try {
+			fechaNacimiento = new SimpleDateFormat("yyyy-MM-dd").parse(fecha);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Usuario usuario = new Usuario();
+		
+		usuario.setNombre(nombre);
+		usuario.setApellido(apellidos);
+		usuario.setDni(dni);
+		usuario.setEmail(email);
+		usuario.setContrasena(contrasena);
+		usuario.setTelefono(telf);
+		usuario.setFechaNacimiento(fechaNacimiento);
+		
+		ModeloUsuario modeloUsuario = new ModeloUsuario();
+		modeloUsuario.conectar();
+		
+		modeloUsuario.insertarUsuario(usuario);
+		
+		usuario = modeloUsuario.getUsuariosViaDNI(dni);
+		
+		modeloUsuario.cerrar();
+		
+		if (usuario != null) {
+			HttpSession session = request.getSession();
+			
+			session.setAttribute("usuario", usuario);
+			
+			response.sendRedirect("VistaUsuario");
+		}else {
+			response.sendRedirect("Registro");
+		}
+		
+		
 	}
 
 }
