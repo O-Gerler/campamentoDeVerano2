81a1a5051b4ca8371be318b04006e600d1a82f5b
O-Gerler <azurmendi5o@outlook.com>
2023-05-13 18:04:34 +0200

    javadoc

--- src/main/java/modeloDAO/ModeloActividad.java
+++ src/main/java/modeloDAO/ModeloActividad.java
@@ -8,6 +8,12 @@ import java.util.ArrayList;
 import modeloDTO.Actividad;
 
 public class ModeloActividad extends Conector{
+	/**
+	 * Metodo para insertar una actividad en la BBDD
+	 * @param actividad -> Es la actividad que se quiere insertar en la BBDD
+	 * @return true -> En caso de que la operacion se haya realizado correctamente
+	 * @return false -> En caso de que haya habido algun error
+	 */
 	public boolean insertarActividad(Actividad actividad) {
 		String st = "INSERT INTO actividades (id_zona, nombre, cantidad_max, edad_min) VALUES (?,?,?,?)";
 		
@@ -28,7 +34,12 @@ public class ModeloActividad extends Conector{
 		
 		return false;
 	}
-	
+	/**
+	 * Metodo para eliminar una actividad en la BBDD
+	 * @param id -> Es el id de la actividad que se quiere eliminar en la BBDD
+	 * @return true -> En caso de que la operacion se haya realizado correctamente
+	 * @return false -> En caso de que haya habido algun error
+	 */
 	public boolean eliminarActividad(int id) {
 		String st = "DELETE FROM actividades WHERE id_actividad=?";
 		
@@ -47,6 +58,12 @@ public class ModeloActividad extends Conector{
 		return false;
 	}
 	
+	/**
+	 * Metodo para modificar una actividad en la BBDD
+	 * @param id -> Es la actividad que se quiere modificar en la BBDD
+	 * @return true -> En caso de que la operacion se haya realizado correctamente
+	 * @return false -> En caso de que haya habido algun error
+	 */
 	public boolean modificarActividad(Actividad actividad) {
 		String st = "UPDATE actividades SET id_zona=?,nombre=?,cantidad_max=?,edad_min=? WHERE id_actividad=?";
 		
@@ -69,6 +86,13 @@ public class ModeloActividad extends Conector{
 		return false;
 	}
 	
+	/**
+	 * Obtener una actividad de la BBDD
+	 * @param id -> Es el id de la actividad que se quiere obtener de la BBDD
+	 * @return Actividad -> En caso de que la actividad exista
+	 * @return null -> En caso de que no exista o de algun error
+	 * @see rellenarActividades
+	 */
 	public Actividad getActividad(int id) {
 		String st = "SELECT * FROM actividades WHERE id_actividad=?";
 		
@@ -91,6 +115,12 @@ public class ModeloActividad extends Conector{
 		return null;
 	}
 
+	/**
+	 * Dado el ResultSet rellena un objeto Actividad y lo devuelve
+	 * @param rs -> Resultset a recorrer con la informacion de la BBDD
+	 * @return -> La actividad ya rellena
+	 * @throws SQLException -> En caso de no encontrar algo u otro error
+	 */
 	private Actividad rellenarActividad(ResultSet rs) throws SQLException {
 		ModeloZona modeloZona = new ModeloZona();
 		modeloZona.conectar();
@@ -108,6 +138,11 @@ public class ModeloActividad extends Conector{
 		return actividad;
 	}
 	
+	/**
+	 * Obtiene todas las actividades de la BBDD
+	 * @return -> Retorna un ArrayList de actividades de la BBDD
+	 * @see rellenarActividad
+	 */
 	public ArrayList<Actividad> getAllActividades() {
 		String st = "SELECT * FROM actividades";
 		ArrayList<Actividad> actividades = new ArrayList<>();
