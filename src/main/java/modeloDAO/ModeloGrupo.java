02e43b69f0d23fe3cb9e767d5de3f48109822c95
O-Gerler <azurmendi5o@outlook.com>
2023-05-14 10:29:30 +0200

    jjavadoc

--- src/main/java/modeloDAO/ModeloGrupo.java
+++ src/main/java/modeloDAO/ModeloGrupo.java
@@ -8,6 +8,12 @@ import java.util.ArrayList;
 import modeloDTO.Grupo;
 
 public class ModeloGrupo extends Conector{
+	/**
+	 * Inserta un nuevo grupo en la base de datos con el monitor especificado.
+	 * 
+	 * @param id_monitor el ID del monitor asignado al grupo.
+	 * @return true si se insertó el grupo correctamente, false en caso contrario.
+	 */
 	public boolean insertarGrupo(int id_monitor) {
 		String st = "INSERT INTO grupos (id_monitor) VALUES (?)";
 		
@@ -26,6 +32,12 @@ public class ModeloGrupo extends Conector{
 		return false;
 	}
 	
+	/**
+	 * Elimina un grupo de la base de datos.
+	 * 
+	 * @param id_grupo el id del grupo a eliminar
+	 * @return true si se eliminó correctamente, false en caso contrario
+	 */
 	public boolean eliminarGrupo(int id_grupo) {
 		String st = "DELETE FROM grupos WHERE id_grupo=?";
 		
@@ -44,6 +56,11 @@ public class ModeloGrupo extends Conector{
 		return false;
 	}
 	
+	/**
+	 * Modifica un grupo en la base de datos.
+	 * @param grupo El grupo a modificar.
+	 * @return true si la modificación fue exitosa, false en caso contrario.
+	 */
 	public boolean modificarGrupo(Grupo grupo) {
 		String st = "UPDATE grupos SET id_monitor=? WHERE id_grupo=?";
 		
@@ -63,6 +80,12 @@ public class ModeloGrupo extends Conector{
 		return false;
 	}
 	
+	/**
+	 * Devuelve el objeto Grupo correspondiente al id especificado.
+	 * 
+	 * @param id El id del grupo a buscar.
+	 * @return El objeto Grupo correspondiente al id especificado, o null si no se encuentra.
+	 */
 	public Grupo getGrupo(int id) {
 		String st = "SELECT * FROM grupos WHERE id_grupo=?";
 		
@@ -85,6 +108,13 @@ public class ModeloGrupo extends Conector{
 		return null;
 	}
 
+	/**
+	 * Rellena un objeto Grupo a partir de los datos obtenidos de un ResultSet.
+	 *
+	 * @param rs ResultSet con los datos del grupo.
+	 * @return Grupo objeto Grupo rellenado con los datos obtenidos del ResultSet.
+	 * @throws SQLException si ocurre alguna excepción al acceder a los datos del ResultSet.
+	 */
 	private Grupo rellenarGrupo(ResultSet rs) throws SQLException {
 		Grupo grupo = new Grupo();
 		
@@ -99,6 +129,13 @@ public class ModeloGrupo extends Conector{
 		return grupo;
 	}
 	
+	
+	/**
+	 * Obtiene todos los grupos que no tienen asignada ninguna parcela.
+	 * 
+	 * @return ArrayList de objetos Grupo sin parcela asignada.
+	 *         Retorna null si ocurre una excepción de tipo SQLException.
+	 */
 	public ArrayList<Grupo> getGruposSinParcela() {
 		String st = "SELECT * FROM grupos WHERE id_grupo not in (SELECT id_grupo FROM parcelas)";
 		ArrayList<Grupo> grupos = new ArrayList<>();
@@ -120,6 +157,11 @@ public class ModeloGrupo extends Conector{
 		return null;
 	}
 	
+	/**
+	 * Retorna todos los grupos de la base de datos.
+	 *
+	 * @return un ArrayList con todos los grupos de la base de datos.
+	 */
 	public ArrayList<Grupo> getAllGrupos() {
 		String st = "SELECT * FROM grupos";
 		ArrayList<Grupo> grupos = new ArrayList<>();
