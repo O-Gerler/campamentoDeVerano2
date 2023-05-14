5d7b40bc299ffa0744e2cca96e71a8980af9e32a
O-Gerler <azurmendi5o@outlook.com>
2023-05-14 11:27:13 +0200

    javadoc

--- src/main/java/modeloDAO/ModeloZona.java
+++ src/main/java/modeloDAO/ModeloZona.java
@@ -8,6 +8,12 @@ import java.util.ArrayList;
 import modeloDTO.Zona;
 
 public class ModeloZona extends Conector{
+	/**
+	 * Inserta una nueva zona en la base de datos.
+	 *
+	 * @param zona el objeto Zona que se va a insertar en la base de datos
+	 * @return verdadero si la inserción fue exitosa, falso en caso contrario
+	 */
 	public boolean insertarZona(Zona zona) {
 		String st = "INSERT INTO zonas(nombre, descripcion) VALUES (?,?)";
 		
@@ -27,6 +33,12 @@ public class ModeloZona extends Conector{
 		return false;
 	}
 	
+	/**
+	 * Elimina una zona de la base de datos.
+	 *
+	 * @param id el ID de la zona que se va a eliminar
+	 * @return verdadero si la eliminación fue exitosa, falso en caso contrario
+	 */
 	public boolean eliminarZona(int id) {
 		String st = "DELETE FROM zonas WHERE id_zona=?";
 		
@@ -45,6 +57,12 @@ public class ModeloZona extends Conector{
 		return false;
 	}
 	
+	/**
+	 * Modifica la información de una zona en la base de datos.
+	 *
+	 * @param zona el objeto Zona con la información actualizada
+	 * @return verdadero si la modificación fue exitosa, falso en caso contrario
+	 */
 	public boolean modificarZona(Zona zona) {
 		String st = "UPDATE zonas SET nombre=?, descripcion=? WHERE id_zona=?";
 		
@@ -65,6 +83,12 @@ public class ModeloZona extends Conector{
 		return false;
 	}
 	
+	/**
+	 * Obtiene una zona de la base de datos a partir de su ID.
+	 *
+	 * @param id el ID de la zona que se quiere obtener
+	 * @return un objeto Zona con la información de la zona solicitada
+	 */
 	public Zona getZona(int id) {
 		String st = "SELECT * FROM zonas WHERE id_zona=?";
 		
@@ -87,6 +111,13 @@ public class ModeloZona extends Conector{
 		return null;
 	}
 
+	/**
+	 * Este método toma un objeto ResultSet y lo utiliza para crear y devolver un objeto Zona.
+	 * 
+	 * @param rs el objeto ResultSet que contiene los datos de la zona
+	 * @return un objeto Zona con los datos obtenidos del ResultSet
+	 * @throws SQLException si ocurre un error al acceder a los datos del ResultSet
+	 */
 	private Zona rellenarZona(ResultSet rs) throws SQLException {
 		Zona zona = new Zona();
 		
@@ -97,6 +128,11 @@ public class ModeloZona extends Conector{
 		return zona;
 	}
 	
+	/**
+	 * Este método recupera todas las zonas de la base de datos y las devuelve en un ArrayList.
+	 * 
+	 * @return un ArrayList de objetos Zona que contiene todas las zonas de la base de datos
+	 */
 	public ArrayList<Zona> getAllZonas() {
 		String st = "SELECT * FROM zonas";
 		ArrayList<Zona> zonas = new ArrayList<>();
