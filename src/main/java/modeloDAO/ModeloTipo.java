c6264960d6487414b12681232abdab395e3eee6c
O-Gerler <azurmendi5o@outlook.com>
2023-05-14 11:07:38 +0200

    javadoc

--- src/main/java/modeloDAO/ModeloTipo.java
+++ src/main/java/modeloDAO/ModeloTipo.java
@@ -8,6 +8,12 @@ import java.util.ArrayList;
 import modeloDTO.Tipo;
 
 public class ModeloTipo extends Conector{
+	/**
+	 * Este método inserta un nuevo tipo en la base de datos.
+	 *
+	 * @param tipo El objeto Tipo que se quiere insertar en la base de datos.
+	 * @return Un valor booleano que indica si la inserción fue exitosa (true) o no (false).
+	 */
 	public boolean insertarTipo(Tipo tipo) {
 		String st = "INSERT INTO tipos (nombre, cantidad_personas, descripcion) VALUES (?, ? ,?)";
 		
@@ -28,6 +34,12 @@ public class ModeloTipo extends Conector{
 		return false;
 	}
 	
+	/**
+	 * Este método elimina un tipo de la base de datos.
+	 *
+	 * @param id El ID del tipo que se quiere eliminar.
+	 * @return Un valor booleano que indica si la eliminación fue exitosa (true) o no (false).
+	 */
 	public boolean eliminarTipo(int id) {
 		String st = "DELETE FROM tipos WHERE id_tipo = ?";
 		
@@ -46,6 +58,12 @@ public class ModeloTipo extends Conector{
 		return false;
 	}
 	
+	/**
+	 * Este método modifica un tipo en la base de datos.
+	 *
+	 * @param tipo El objeto Tipo que contiene los datos actualizados.
+	 * @return Un valor booleano que indica si la modificación fue exitosa (true) o no (false).
+	 */
 	public boolean modificarTipo(Tipo tipo) {
 		String st = "UPDATE tipos SET nombre=?, cantidad_personas=?, descripcion=? WHERE id_tipo=?";
 		
@@ -67,6 +85,12 @@ public class ModeloTipo extends Conector{
 		return false;
 	}
 	
+	/**
+	 * Este método devuelve un objeto Tipo de la base de datos.
+	 *
+	 * @param id El ID del tipo que se quiere obtener.
+	 * @return Un objeto Tipo con los datos del tipo especificado.
+	 */
 	public Tipo getTipo(int id) {
 		String st = "SELECT * FROM tipos WHERE id_tipo = ?";
 		
@@ -89,6 +113,13 @@ public class ModeloTipo extends Conector{
 		return null;
 	}
 
+	/**
+	 * Este método privado toma un objeto ResultSet y devuelve un objeto Tipo.
+	 *
+	 * @param rs El objeto ResultSet que contiene los datos del tipo.
+	 * @return Un objeto Tipo con los datos del ResultSet.
+	 * @throws SQLException Si ocurre un error al acceder a los datos del ResultSet.
+	 */
 	private Tipo rellenarTipo(ResultSet rs) throws SQLException {
 		Tipo tipo = new Tipo();
 		
@@ -99,6 +130,11 @@ public class ModeloTipo extends Conector{
 		return tipo;
 	}
 	
+	/**
+	 * Este método devuelve una lista de todos los tipos.
+	 *
+	 * @return Una lista de objetos Tipo que contiene todos los tipos.
+	 */
 	public ArrayList<Tipo> getAllTipos() {
 		String st = "SELECT * FROM tipos";
 		ArrayList<Tipo> tipos = new ArrayList<>();
