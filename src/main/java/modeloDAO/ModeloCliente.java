40974966b505673284ac922866d699aa89dd61c2
O-Gerler <azurmendi5o@outlook.com>
2023-05-14 10:25:53 +0200

    javadoc

--- src/main/java/modeloDAO/ModeloCliente.java
+++ src/main/java/modeloDAO/ModeloCliente.java
@@ -9,6 +9,11 @@ import modeloDTO.Cliente;
 import modeloDTO.Usuario;
 
 public class ModeloCliente extends Conector{
+	/**
+	 * Retorna un objeto Cliente a partir de un objeto Usuario.
+	 * @param id El id del usuario.
+	 * @return El objeto Cliente obtenido.
+	 */
 	private Cliente clienteHeredaUsuario(int id) {
 		ModeloUsuario modeloUsuario = new ModeloUsuario();
 		modeloUsuario.conectar();
@@ -30,6 +35,11 @@ public class ModeloCliente extends Conector{
 		return cliente;
 	}
 	
+	/**
+	 * Inserta un nuevo cliente en la base de datos.
+	 * @param cliente El cliente a insertar.
+	 * @return true si el cliente se insertó correctamente, false en caso contrario.
+	 */
 	public boolean insertarCliente(Cliente cliente) {
 		String st = "INSERT INTO clientes VALUES (?,?)";
 		
@@ -49,6 +59,12 @@ public class ModeloCliente extends Conector{
 		return false;
 	}
 
+	/**
+	 * Elimina un cliente de la base de datos.
+	 *
+	 * @param id el ID del cliente a eliminar.
+	 * @return true si se eliminó correctamente, false si ocurrió un error.
+	 */
 	public boolean eliminarCliente(int id) {
 		String st = "DELETE FROM clientes WHERE id_cliente=?";
 		
@@ -67,6 +83,13 @@ public class ModeloCliente extends Conector{
 		return false;
 	}
 	
+	/**
+	 * Modifica un cliente en la base de datos.
+	 * 
+	 * @param cliente el cliente a modificar.
+	 * @return true si se ha modificado correctamente, false en caso contrario.
+	 */
+
 	public boolean modificarCliente(Cliente cliente) {
 		String st = "UPDATE clientes SET id_grupo=? WHERE id_cliente=?";
 		
@@ -86,6 +109,11 @@ public class ModeloCliente extends Conector{
 		return false;
 	}
 	
+	/**
+	* Devuelve el objeto Cliente correspondiente al id proporcionado.
+	* @param id El id del cliente.
+	* @return El objeto Cliente correspondiente al id proporcionado, o null si no existe.
+	*/
 	public Cliente getCliente(int id) {
 		String st = "SELECT * FROM clientes WHERE id_cliente=?";
 		
@@ -108,6 +136,12 @@ public class ModeloCliente extends Conector{
 		return null;
 	}
 
+	/**
+	 * Rellena los datos de un objeto Cliente a partir de un objeto ResultSet de una consulta a la tabla "clientes" de la base de datos.
+	 * @param rs Objeto ResultSet con los datos del cliente consultado.
+	 * @return Objeto Cliente con los datos rellenados a partir del ResultSet.
+	 * @throws SQLException Si ocurre algún error en la consulta SQL.
+	 */
 	private Cliente rellenarCliente(ResultSet rs) throws SQLException {
 		Cliente cliente = clienteHeredaUsuario(rs.getInt("id_cliente"));
 		
@@ -121,6 +155,14 @@ public class ModeloCliente extends Conector{
 		return cliente;
 	}
 	
+	/**
+	 * Devuelve una lista de clientes asociados a un monitor determinado.
+	 * 
+	 * @param id_monitor el ID del monitor del que se desea obtener la lista de clientes.
+	 * @return una lista de objetos Cliente asociados al monitor especificado.
+	 *         Si no se encuentra ningún cliente, devuelve una lista vacía.
+	 *         Si ocurre una excepción SQL, devuelve null.
+	 */
 	public ArrayList<Cliente> getClientesPorMonitor(int id_monitor) {
 		String st = "SELECT * FROM clientes WHERE id_grupo = (SELECT id_grupo FROM grupos WHERE id_monitor=?)";
 		ArrayList<Cliente> clientes = new ArrayList<>();
@@ -144,6 +186,12 @@ public class ModeloCliente extends Conector{
 		return null;
 	}
 	
+	/**
+	 * Obtiene todos los clientes almacenados en la base de datos.
+	 * 
+	 * @return un ArrayList de objetos Cliente con la información de todos los clientes, 
+	 *         o null si ocurre un error al acceder a la base de datos.
+	 */
 	public ArrayList<Cliente> getAllClientes() {
 		String st = "SELECT * FROM clientes";
 		ArrayList<Cliente> clientes = new ArrayList<>();
